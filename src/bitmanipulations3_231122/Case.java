eb351820dc6afcbdffab25721917d630fe4380d8
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-04-27 21:31:45 +0530

    bitmanipulation

--- src/bitmanipulations3_231122/Case.java
+++ src/bitmanipulations3_231122/Case.java
@@ -1,4 +1,4 @@
-package array231122;
+package bitmanipulations3_231122;
 
 public class Case {
 // https://stackoverflow.com/questions/3884793/why-is-double-min-value-in-not-negative
@@ -13,6 +13,17 @@ public class Case {
 		System.out.println(Double.MIN_VALUE); // 4.9E-324
 		System.out.println(Double.NEGATIVE_INFINITY); // -Infinity
 		System.out.println(-Double.MAX_VALUE); // -1.7976931348623157E308
+		int a = 100000; int b = 1000000;
+		int c = a*b;
+		System.out.println(c); // 1215752192 -- overflow
+		long d = a*b;
+		System.out.println(d); // 1215752192 -- overflow
+		long e = (long)(a*b);
+		System.out.println(e); // 1215752192 -- overflow
+		long f = (long)(a)*b;
+		System.out.println(f); // 100000000000 -- no overflow
+		long g = (long)(a)*(long)(b);
+		System.out.println(g); // 100000000000 -- no overflow
 		
 
 	}
