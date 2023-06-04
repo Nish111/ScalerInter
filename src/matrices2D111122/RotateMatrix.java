cdbaedcd15e1fb761a04434445789c8c07f18bcf
Nishant Banginwar <banginwar.nishant1@gmail.com>
2023-06-04 14:33:17 +0530

    array

--- src/matrices2D111122/RotateMatrix.java
+++ src/matrices2D111122/RotateMatrix.java
@@ -1,5 +1,5 @@
-package array111122;
-
+package matrices2D111122;
+// https://www.scaler.com/academy/mentee-dashboard/class/40895/assignment/problems/55?navref=cl_tt_nv
 public class RotateMatrix {
 	public void solve(int[][] A) {
         int N = A.length;
@@ -14,12 +14,7 @@ public class RotateMatrix {
 				A[j][i] = temp;
 			}	
 		}
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				System.out.print(A[i][j] + " ");
-			}
-			System.out.println();
-		}
+		printArray(A, N);
         // reverse or swap cols
         for(int i=0; i<N; i++){
             for(int j=0; j<N/2; j++){
@@ -29,13 +24,16 @@ public class RotateMatrix {
                 //System.out.println(A[i][j] + " xx " + A[0][N-1-j] + " yy ");
             }
         }
-        for(int i=0; i<N; i++) {
+        printArray(A, N);
+    }
+	private void printArray(int[][] A, int N) {
+		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
 				System.out.print(A[i][j] + " ");
 			}
 			System.out.println();
 		}
-    }
+	}
 	public static void main(String[] args) {
 		RotateMatrix rm = new RotateMatrix();
 		int[][] A = {{1,2}, {3,4}}; 
